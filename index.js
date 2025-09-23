9671cba00de117c17d5d0c05562616ce5f88e1db
Nimra <nimraiqbal512@gmail.com>
2025-09-23 15:28:02 +0500

    Lecture 24 - Express boiler plate with basic setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Ya ALi a.s Madad');
+});
