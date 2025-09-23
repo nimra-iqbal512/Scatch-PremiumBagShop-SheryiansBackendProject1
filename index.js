2d3ff96efc2c5fb7d1dbe7f15336f05d6c62db98
Nimra <nimraiqbal512@gmail.com>
2025-09-23 21:20:18 +0500

    Lecture 24 - creating owners, products and users Routers and linking them with index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const ownersRouter = require('./routes/ownersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
 
 const db = require('./config/mongoose-connection');
 
@@ -11,9 +14,9 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/owners', ownersRouter); //owners k related sari requests ownerRouter py bhej di jaye.
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 app.listen(3000, () => {
   console.log('Ya ALi a.s Madad');
