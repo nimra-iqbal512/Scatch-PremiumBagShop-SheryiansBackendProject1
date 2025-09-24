5a2f712b15674aa88ee0d9e5084990cee710d423
Nimra <nimraiqbal512@gmail.com>
2025-09-24 14:19:59 +0500

    Lecture 26 - Debugger setup

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const dbgr = require("debug")("development:mongoose"); //After requiring the package, we can define namespace(i.e. development, production, etc) here.
 
 mongoose.connect(`mongodb://127.0.0.1:27017/scatch`)    //This string is only for local connection. Will not work when hosted online
 .then(function(){
-    console.log("Database connected"); 
+    dbgr("Database connected"); 
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 });
 
 module.exports = mongoose.connection;
