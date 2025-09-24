e1b3b9479f8baf38ca253f417a06c3e411aa6905
Nimra <nimraiqbal512@gmail.com>
2025-09-24 14:47:55 +0500

    Lecture 26 - Securing MongoDB connection - mongoose.connect

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const config = require('config');
+
 const dbgr = require("debug")("development:mongoose"); //After requiring the package, we can define namespace(i.e. development, production, etc) here.
 
-mongoose.connect(`mongodb://127.0.0.1:27017/scatch`)    //This string is only for local connection. Will not work when hosted online
-.then(function(){
-    dbgr("Database connected"); 
-})
-.catch(function(err){
-    dbgr(err);
-});
+mongoose
+    .connect(`${config.get("MONGODB_URI")}/scatch`)  
+    .then(function(){
+        dbgr("Database connected"); 
+    })
+    .catch(function(err){
+        dbgr(err);
+    });
 
 module.exports = mongoose.connection;
